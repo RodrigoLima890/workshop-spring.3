3606835d9e6492f36cdd4f8044756e0eb058a965
RodrigoLima890 <rodrigodaschagas1234@gmail.com>
2022-11-23 11:27:55 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/minhaApiWeb/demo/entities/Product.java
+++ src/main/java/com/minhaApiWeb/demo/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_product")
@@ -31,6 +35,9 @@ public class Product implements Serializable{
 	joinColumns = @JoinColumn(name = "product_id"),
 	inverseJoinColumns = @JoinColumn(name = "category_Id"))
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Product() {}
 	
@@ -84,6 +91,14 @@ public class Product implements Serializable{
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	@JsonIgnore
+	public Set<Order> getOrder(){
+		Set<Order> orders = new HashSet<>();
+		for(OrderItem item : items) {
+			orders.add(item.getOrder());
+		}
+		return orders;
 	}
 
 	@Override
