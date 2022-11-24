e39d2f6581ae836631e851c65b81a90fd503671b
RodrigoLima890 <rodrigodaschagas1234@gmail.com>
2022-11-24 10:18:20 -0300

    User insert

--- src/main/java/com/minhaApiWeb/demo/resources/UserResource.java
+++ src/main/java/com/minhaApiWeb/demo/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.minhaApiWeb.demo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.minhaApiWeb.demo.entities.User;
 import com.minhaApiWeb.demo.service.UserService;
@@ -27,6 +31,12 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable(name = "userId")  Long id){
 		User user = service.findUserById(id);
 		return ResponseEntity.ok().body(user);
+	}
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 
 }
